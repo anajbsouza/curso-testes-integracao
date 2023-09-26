b85d47e56ab91dfe28fabf9a08d6fbfef927fbbc
anajbsouza <anaju.batsouza@gmail.com>
2023-09-26 17:19:34 -0300

    ex 3

---
+++ aula-testes-03/tests/calculator.test.ts
@@ -0,0 +1,14 @@
+describe("quatro operações básicas da matemática", () => {
+    it("deve retornar a soma de dois valores", () => {
+        expect(2+3).toBe(5);
+    });
+    it("deve retornar a subtração de dois valores", () => {
+        expect(2-1).toBe(1);
+    });
+    it("deve retornar a divisão de dois valores", () => {
+        expect(4/2).toBe(2);
+    });
+    it("deve retornar a multiplicação de dois valores", () => {
+        expect(2*5).toBe(10);
+    });
+})
